8fa904de20c33073051362537b5dd2a66bf833df
Mário C <igotbugs@ticklemynausea.net>
2016-10-17 00:13:19 +0100

    load multiple scenes each from its own file
    also added new scenes

--- app/director.js
+++ app/director.js
@@ -4,16 +4,25 @@
 
     const Scene = require('./scene');
 
-    let Director = function(script, canvas) {
+    let Director = function(canvas) {
 
-        this.script = script;
         this.canvas = canvas;
+
+        /* load scenes */
+        const requireScenes = function (requireContext) {
+            return requireContext.keys().reduce(function(accumulator, filename) {
+                let framename = filename.match(/\.\/(\S+)\.json$/)[1];
+                accumulator[framename] = requireContext(filename);
+                return accumulator;
+            }, {});
+        };
+        this.scenes = requireScenes(require.context("../assets/scenes", true, /\.json$/));
 
     };
 
     Director.prototype.begin = function() {
-
-        let scene = new Scene(this.script.scenes.vaginal, this.canvas);
+        console.log(this.scenes);
+        let scene = new Scene(this.scenes.anal, this.canvas);
 
         scene.play()
             .then(() => console.log('done'))
--- app/index.js
+++ app/index.js
@@ -4,13 +4,11 @@
 
     require('../assets/style.css');
 
-    const script = require('./script.json');
-
     const Canvas = require('./canvas');
     const Director = require('./director');
 
     let canvas = new Canvas(document.getElementById("canvasDiv"), 512, 272);
-    let director = new Director(script, canvas);
+    let director = new Director(canvas);
 
     director.begin();
 
