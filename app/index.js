0374b0038f40f950af8804d4684f7494acc26666
Mário C <igotbugs@ticklemynausea.net>
2016-10-17 02:24:46 +0100

    add game title gif to page

--- app/index.js
+++ app/index.js
@@ -2,7 +2,14 @@
 
     'use strict';
 
+    /* css stuff */
     require('../assets/style.css');
+
+    /* load title image into title div because im using a stupid fucking
+     * plugin to generate index.html automagically and i dont know how
+     * pass content to the template */
+    const image = require('../assets/page/paradise.gif');
+    document.getElementById('titleImg').src = image;
 
     /* Canvas abstraction */
     const Canvas = require('./canvas');
